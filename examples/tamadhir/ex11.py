26bda1325c246316c861df341f6401da38ec215f
Tamadhir10 <tamadhir.alhabsi@gmail.com>
2025-03-02 12:43:33 +0400

    Adding countdown culcolation

--- examples/tamadhir/ex11.py
+++ examples/tamadhir/ex11.py
@@ -1,3 +1,11 @@
+
 print("Countdown Task")
 
-num = input(int("Enter the Number: "))
+num = int(input("Enter the Number: "))
+
+while num > 0:
+    print(num, end=" ")
+    num -= 1
+print(0)    
+
+    

1fd08dbb6a815f2fc6e3ed0db6498c2c60e8fd52
khitam99 <khitam9955@gmail.com>
2025-03-03 11:45:42 +0400

    Function

---
+++ examples/khitam/expense_manager.py
@@ -0,0 +1,15 @@
+def print_clothiong_pattern(given_num):
+    print("================================")
+    print(f"******** clothingm patteren {given =_num}*********")
+    print("================================")
+
+print_clothiong_pattern(given_num=1)
+def sum(num1,num2):
+    result =num1+num2
+    return result 
+
+print(sum(1,3))
+print(sum(5,4))
+print(sum(15,6))
+
+print("Exiting!")

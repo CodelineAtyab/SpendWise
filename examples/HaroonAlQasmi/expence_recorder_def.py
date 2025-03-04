87c721688b82eaa5682229bd4f986fb87b772f42
HaroonAlQasmi <hhhaaroonn13@gmail.com>
2025-03-04 10:35:07 +0400

    Updated the changes

--- examples/HaroonAlQasmi/expence_recorder_def.py
+++ examples/HaroonAlQasmi/expence_recorder_def.py
@@ -1,6 +1,9 @@
 from datetime import datetime
+
 expenses_dictionary = {}
-item_id = 1
+item_id = 1 # this is used as an id for the user inputs and it will incremented later for future inputs
+
+
 def add_expense():
     global expenses_dictionary, item_id
     expense = input("Enter amount: ")
@@ -9,16 +12,25 @@ def add_expense():
     expenses_dictionary[item_id] = [customer_name, expense, current_datetime]
     print(f"Expense added with ID: {item_id}")
     item_id += 1
+
+
 def view_expenses():
-    print(expenses_dictionary)
+    for key, value in expenses_dictionary.items(): # this for loop is used to extract the data from the dictionary
+        customer_name = value[0]
+        amount = value[1]
+        date_time = value[2].strftime("%#m/%#d/%Y %H:%M")
+    print(f"Id: {key}, Customer Name: {customer_name}, Amount: {amount} OMR, Date-Time: {date_time}") # this is used to print a humanly readable string
+
+
 def update_expense():
-    global expenses_dictionary
     expense_index = int(input("Enter index number 'the number on the left': "))
     updated_expense = input("Enter the updated amount: ")
     updated_customer_name = input("Enter updated customer name: ")
     current_datetime = datetime.now()
     expenses_dictionary[expense_index] = [updated_customer_name, updated_expense, current_datetime]
     print("Expense updated.")
+
+    
 def remove_expense():
     global expenses_dictionary
     expense_index = int(input("Enter index number 'the number on the left': "))

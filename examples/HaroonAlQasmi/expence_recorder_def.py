efc1248233bac4ac7f7b0320d2a23f7658b451f4
HaroonAlQasmi <hhhaaroonn13@gmail.com>
2025-03-04 11:03:21 +0400

    Updated the changes

--- examples/HaroonAlQasmi/expence_recorder_def.py
+++ examples/HaroonAlQasmi/expence_recorder_def.py
@@ -1,17 +1,16 @@
 from datetime import datetime
 
 expenses_dictionary = {}
-item_id = 1 # this is used as an id for the user inputs and it will incremented later for future inputs
 
 
 def add_expense():
-    global expenses_dictionary, item_id
+    global expenses_dictionary
     expense = input("Enter amount: ")
     customer_name = input("Enter customer name: ")
+    customer_contact = input("Enter customer contact info:")
     current_datetime = datetime.now()
-    expenses_dictionary[item_id] = [customer_name, expense, current_datetime]
-    print(f"Expense added with ID: {item_id}")
-    item_id += 1
+    expenses_dictionary[customer_contact] = [customer_name, expense, current_datetime]
+    print(f"Expense added with ID: {customer_contact}")
 
 
 def view_expenses():
@@ -19,11 +18,11 @@ def view_expenses():
         customer_name = value[0]
         amount = value[1]
         date_time = value[2].strftime("%#m/%#d/%Y %H:%M")
-    print(f"Id: {key}, Customer Name: {customer_name}, Amount: {amount} OMR, Date-Time: {date_time}") # this is used to print a humanly readable string
+    print(f"Contact Info: {key}, Customer Name: {customer_name}, Amount: {amount} OMR, Date-Time: {date_time}") # this is used to print a humanly readable string
 
 
 def update_expense():
-    expense_index = int(input("Enter index number 'the number on the left': "))
+    expense_index = int(input("Enter Contact info: "))
     updated_expense = input("Enter the updated amount: ")
     updated_customer_name = input("Enter updated customer name: ")
     current_datetime = datetime.now()

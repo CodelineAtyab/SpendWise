7db9a7208791f6cc01748295d2aed70a75ebec89
LeenaAlhabsi <leenahamoodalhabsi@gmail.com>
2025-03-04 09:55:53 +0400

    update the program

--- examples/leena/expenses/expense_utils.py
+++ examples/leena/expenses/expense_utils.py
@@ -6,7 +6,15 @@ TODO: To save everything in a persistent storage like DB
 _expense_data_list = {}
 
 def add(email, amount_list):
-    _expense_data_list[email] = amount_list
+    if email in _expense_data_list:
+        print(f"\nRecord for {email} already exists. Updating the record.")
+        existing_amount = _expense_data_list[email][0]
+        new_amount = existing_amount + amount_list[0]
+        number_of_loans = _expense_data_list[email][3] + 1
+        _expense_data_list[email] = [new_amount, amount_list[1], amount_list[2], number_of_loans]
+    else:
+        amount_list.append(1)  # Initialize the number of loans to 1
+        _expense_data_list[email] = amount_list
     return True
 
 def delete(item_key):
@@ -21,4 +29,3 @@ def get_all():
 def update(email, amount_list):
     _expense_data_list[email] = amount_list
     return True
-    

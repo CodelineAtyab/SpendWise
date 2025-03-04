8dea31725a738844954eb0df7241881c81850102
Hamedalamri99 <hamed.y99k@gmail.com>
2025-03-04 10:56:04 +0400

    New ubdating with following format

--- examples/Hamed/main.py
+++ examples/Hamed/main.py
@@ -1,6 +1,9 @@
 from expense_tracker import ExpenseTracker
 
 def main():
+
+    """The tracker use to..........  """
+
     tracker = ExpenseTracker()
 
     while True:
@@ -15,43 +18,47 @@ def main():
 
         choice = input("Choose an option: ")
 
+        if choice in ["1", "2", "3"]:
+            email = input("Enter your email address: ").strip().lower()
+            if "@" not in email or "." not in email or "com" not in email:
+                print("Invalid email. Please enter a valid email address with corect formating\nThank you: ")
+                continue
+
         if choice == "1":
             description = input("Enter expense description: ")
             try:
                 amount = float(input("Enter expense amount: "))
-                tracker.add_expense(description, amount)
+                tracker.add_expense(email, description, amount)
                 print("Expense added successfully.")
             except ValueError:
                 print("Invalid amount. Please enter a valid number.")
         
         elif choice == "2":
-            tracker.view_expenses()
+            tracker.view_expenses(email)
         
         elif choice == "3":
-            tracker.view_expenses()
+            tracker.view_expenses(email)
             try:
                 index = int(input("Enter the index of the expense to update: "))
                 new_description = input("Enter new description: ")
                 new_amount = float(input("Enter new amount: "))
-                tracker.update_expense(index, new_description, new_amount)
+                tracker.update_expense(email, index, new_description, new_amount)
                 print("Expense updated successfully.")
             except ValueError:
                 print("Invalid input. Please enter a valid index and amount.")
         
         elif choice == "4":
-            tracker.view_expenses()
+            tracker.view_expenses(email)
             try:
                 index = int(input("Enter the index of the expense to delete: "))
                 confirm = input(f"Are you sure you want to delete expense {index}? (y/n): ").lower()
                 if confirm == 'y':
-                    tracker.delete_expense(index)
-                    print(f"Expense {index} deleted successfully.")
+                    tracker.delete_expense(email, index)
                 else:
                     print("Deletion canceled.")
             except ValueError:
                 print("Invalid input. Please enter a valid index.")
-            except IndexError:
-                print("Invalid index. Please select an existing expense.")
+            
         
         elif choice == "5":
             print("Exiting Expense Tracker. Thank you, Goodbye!")

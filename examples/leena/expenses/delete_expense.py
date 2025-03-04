f08ef804015ef494e9caf3a0f72015ab67c1cf43
LeenaaAlhabsi <leenahamoodalhabsi@gmail.com>
2025-03-04 11:39:25 +0400

    Feature/86erpv0um data structuer (#27)
    * hash table
    
    * correct errors
    
    * correct errors
    
    * final copy of task
    
    * update the program
    
    * update the program
    
    * new change

---
+++ examples/leena/expenses/delete_expense.py
@@ -0,0 +1,9 @@
+import expense_utils
+
+def delete_expense(item_key,loan_index):
+    if expense_utils.delete(item_key,loan_index):
+        print("")
+        print("Expense deleted successfully!")
+        print("")
+    else:
+        print("Expense not found!")

ab846a48a66a43b71dc6640e93e1f88a551a2b11
Syed Atyab Hussain <syed.atyab.hussain.om@gmail.com>
2025-03-05 11:47:11 +0400

    added a server app that can call functions with arguments

--- examples/atyab/expense_cli_app/server_app.py
+++ examples/atyab/expense_cli_app/server_app.py
@@ -1,4 +1,16 @@
-import subprocess
+import uvicorn
 
-res = subprocess.run(["python", "main_app.py", "view-amount"], capture_output=True, text=True)
-print(res.stdout)
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/my_func")
+def my_func():
+  return {"message": "Hello, Team Abtal A-Digital!!!"}
+
+@app.get("/sum")
+def sum(num1, num2):
+  return {"result": int(num1) + int(num2)}
+
+if __name__ == "__main__":
+  uvicorn.run(app, host="0.0.0.0", port=8888)

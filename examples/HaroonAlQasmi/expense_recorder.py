48da5beeb4bca4982641739f1ece946a2159e523
HaroonAlQasmi <hhhaaroonn13@gmail.com>
2025-03-26 09:39:28 +0400

    finished task 'verifiying through hashs'

--- examples/HaroonAlQasmi/expense_recorder.py
+++ examples/HaroonAlQasmi/expense_recorder.py
@@ -1,7 +1,4 @@
-from expence_recorder_def import remove_expense, update_expense, view_expenses, add_expense,hash_dictionary,expenses_dictionary,compute_sha256
-import os
-import json
-from datetime import datetime 
+from expence_recorder_def import remove_expense, update_expense, view_expenses, add_expense,update_hash_file,update_expense
 
 hash_file = "hashes.json"
 expenses_file = "expenses.json"
@@ -20,35 +17,16 @@ while app_is_on:
     choice = input("Enter your choice: ")
     if choice == "1":
         add_expense()
+        update_hash_file()
     elif choice == "2":
         view_expenses()
     elif choice == "3":
         update_expense()
+        update_hash_file()
     elif choice == "4":
         remove_expense()
+        update_hash_file()
     elif choice == "5":
-        if os.path.exists(hash_file):
-            with open(hash_file, "r") as json_file:
-                try:
-                    data = json.load(json_file)
-                except json.JSONDecodeError:
-                    data = {}
-        else:
-            data = {}
-        data.update(hash_dictionary)
-        with open(hash_file, "w") as json_file:
-            json.dump(data, json_file, indent=4, default=str)
-        if os.path.exists(expenses_file):
-            with open(expenses_file, "r") as json_file:
-                try:
-                    data = json.load(json_file)
-                except json.JSONDecodeError:
-                    data = {}
-        else:
-            data = {}
-        data.update(expenses_dictionary)
-        with open(expenses_file, "w") as json_file:
-            json.dump(data, json_file, indent=4, default=str)
         app_is_on = False
     else:
         print("Invalid choice. Please try again.")

0f0f3b569f3e9174c054581e775682a5d04f4150
zainab-alghaithi <zainabalghaithi@gmail.com>
2025-04-09 09:57:49 +0400

    reload this file

--- examples/zainab/expense_store.py
+++ examples/zainab/expense_store.py
@@ -1,20 +1,10 @@
-import time
+given_amount = float(input("Enter the amount: "))
 
-# Validate user input to ensure it's a positive number
-while True:
-    try:
-        count_time = float(input("Enter the number of seconds for the countdown: "))
-        if count_time <= 0:
-            print("Please enter a positive number greater than zero.")
-        else:
-            break
-    except ValueError:
-        print("Invalid input! Please enter a valid number.")
+if given_amount > 0:
+    # Store the amount in a file
+    with open("amount.txt", "w") as file:
+        file.write(f"{given_amount}\n")
+    print("Amount Stored in a file.")
 
-# Countdown logic
-while count_time > 0:
-    print(int(count_time))  # Display countdown as integer
-    time.sleep(1)  # Wait for 1 second
-    count_time -= 1
+print(f'The amount is {given_amount}')
 
-print("Time's up!!")
